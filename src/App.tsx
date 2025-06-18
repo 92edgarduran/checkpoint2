eef32fbd67867b61bce7401cfc2a6cd43a46f093
92edgarduran <92edgarduran@gmail.com>
2025-06-17 22:56:53 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Inicio from './pages/Inicio';
+import Noticias from './pages/Noticias';
+import DondeComer from './pages/DondeComer';
+import Eventos from './pages/Eventos';
+import Visitanos from './pages/Visitanos';
+import Contacto from './pages/Contacto';
 
 function App() {
   return (
-    <div>
-      <h1>Bienvenido a mi ciudad 🏙️</h1>
-      <p>Noticias, lugares para comer, eventos y más.</p>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/noticias" element={<Noticias />} />
+          <Route path="/donde-comer" element={<DondeComer />} />
+          <Route path="/eventos" element={<Eventos />} />
+          <Route path="/visitanos" element={<Visitanos />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
